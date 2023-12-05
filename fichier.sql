6fb91c57ca3f500392ccf6ec16a013a8e985f9ad
abdelhakabaha <abdelfak118@gmail.com>
2023-12-05 17:10:13 +0100

    le dirnier modification de projet

--- fichier.sql
+++ fichier.sql
@@ -1,47 +1,95 @@
-drop database electroNacer;
-create database electroNacer;
-use electroNacer;
+drop database Electro_naccer;
+create database Electro_naccer;
+use Electro_naccer;
 
 
 
-create table utilisateur(
-    utlisateur_id int primary key,
-    utilisateur_password varchar(20)
+
+
+create table User (
+    UserId int PRIMARY KEY ,
+   
+    Passwords varchar(20) not null
+   
 );
-describe utilisateur;
-insert into utilisateur(utlisateur_id , utilisateur_password ) 
-values(14 ,'idr' ),
-    (12, 'tilila'),
-    (13, 'arinas'), 
-    (15, 'agrzam');
-select * from utilisateur;
+describe User;
+ INSERT INTO User(UserId, Passwords)
+ VALUES (1,"abc");
+
+ CREATE TABLE Category (
+    Category_Id INT PRIMARY key,
+    Category_nam varchar(20)
+ );
+ insert into Category 
+ values (1,"adruino"),
+  (2,"acsessoire et cablse"),
+  (3,"phones");
+ 
+ create table products (
+    Product_Id int PRIMARY key,
+    Product_name varchar(20),
+    Product_img varchar(50),
+    prix_unitair float,
+    mini_de_stok int, 
+    max_de_stok int,
+    categoryy_ID int,
+    foreign key(categoryy_ID) references Category(Category_Id)
+ );
+
+ insert into products (Product_Id,Product_name,Product_img,prix_unitair,mini_de_stok,max_de_stok,categoryy_ID)
+   values(1, 'Arduino Uno', 'Adruinouno.jfif', 2050, 18, 12, 1),
+ (2, 'Portable Bluetooth Speakers', 'PortableBluetoothSpeakers.jfif', 2380, 30, 130, 2),
+(3, 'Huawei P40 Pro', 'HuaweiP40Pro.jfif', 2740, 10, 210, 3),
 
 
-create table produit(
-     id int primary key,
-     libelle varchar(50),
-     prix_unitaire float,
-     quantite_min int,
-     quantite_stock int,
-     categorie varchar(30)
-);
-describe produit;
-insert into produit ( id, libelle, prix_unitaire, quantite_min, quantite_stock,categorie)
- values(1 , 'machine_electrique' , 123.45, 3, 34, 'electronique'),
-    (2 , 'robo' , 299.00, 1, 56, 'robotique'),
-    (3,  'micanique', 87.76, 7, 78, 'mécanique');
-  select * from produit;
+ (4, 'Arduino Nano', 'ArduinoNano.jfif', 1875, 10, 80, 1),
+
+ 
+
+ (5, 'Arduino Due', 'ArduinoDue.jfif', 1580, 0, 9, 1),
+
+ (6, 'Arduino Pro Mini', 'ArduinoProMini.jfif', 3025, 12, 20, 1),
 
 
 
-  create table catégorie(
-    catégorie_id int primary key,
-    categorie_password varchar(20)
-);
-describe categorie;
-insert into categorie(categorie_id , categorie_password ) 
-values(1) ,'idr' ),
-    (12, 'tilila'),
-    (13, 'arinas'), 
-    (15, 'agrzam');
-select * from categorie;
+ (7, 'Arduino Zero', 'ArduinoZero.jfif', 1790, 7, 10, 1),
+
+ (8, 'Arduino LilyPad', 'ArduinoLilyPad.jfif', 2130, 0, 10, 1),
+
+ (9, 'Arduino Ethernet', 'ArduinoEthernet.jfif', 1945, 20, 70, 1),
+
+ (10, 'Wireless Mouse Keyboard', 'WirelessMouseKeyboard.jfif', 2875, 77, 180, 2),
+
+ (11, 'USB Flash Drives', 'USBFlashDrives.jfif', 2490, 20, 100, 2),
+ (12, 'Arduino Mega 2560', 'ArduinoMega.jfif', 2050, 34, 120, 1),
+
+(13, 'Smartphone Cases Covers', 'SmartphoneCasesCovers.jfif', 1675, 2, 17, 2),
+(14, 'Arduino Leonardo', 'ArduinoLeonardo.jfif', 2500, 49, 150, 1),
+
+ (15, 'Screen Protectors', 'ScreenProtectors.jfif', 2360, 9, 150, 2),
+ (16, 'Vivo X 60 Pro', 'VivoX60Pro.jfif', 2460, 2, 160, 3),
+(17, 'Oppo Find X3 Pro', 'OppoFindX3Pro.jfif', 2510, 6, 11, 3),
+
+
+ (18, 'Micro SD Cards', 'MicroSDCards.jfif', 2080, 7, 12,2),
+
+
+(19, 'External Hard Drives', 'ExternalHardDrives.jfif', 2240, 6, 110, 2),
+(20, 'Ethernet Cables', 'EthernetCables.jfif', 1890, 5, 90, 2),
+(21, 'Power Banks', 'PowerBanks.jfif', 2575, 60, 16, 2),
+(22, 'Webcams', 'Webcams.jfif', 1650, 80, 100, 2),
+(23, 'Wireless Charging Pads', 'WirelessChargingPads.jfif', 3110, 100, 21, 2),
+(24, 'Xiaomi Mi 11', 'XiaomiMi11.jfif', 1860, 21, 110, 3),
+(25, 'Sony Xperia 1 III', 'SonyXperia1III.jfif', 2215, 146, 140, 3),
+(26, 'Samsung Galaxy S21', 'SamsungGalaxyS21.jfif', 2025, 4, 3, 3),
+(27, 'Realme GT', 'RealmeGT.jfif', 2930, 6, 190, 3),
+(28, 'One Plus 9 Pro', 'OnePlus9Pro.jfif', 1750, 7, 90, 3),
+(29, 'Laptop Sleeves Bags', 'LaptopSleevesBags.jfif', 2620, 5, 200, 2),
+
+(30, 'Motorola Edge+', 'MotorolaEdge+.jfif', 2460, 4, 160, 3),
+ (31, 'Arduino MKR1000', 'ArduinoMKR1000.jfif', 2260, 8, 12, 1),
+
+(32, 'iphone 13', 'iphone13.jfif', 2190, 32, 130, 3),
+(33, 'Google Pixel 6', 'GooglePixel6.jfif', 2740, 5, 210, 3),
+(34, 'Huawei P40 Pro', 'HuaweiP40Pro.jfif', 2190, 13, 1, 3),
+(35, 'ASUSROG Phone 5', 'ASUSROGPhone5.jfif', 2740, 22,010, 3);
---
+++ home.php
@@ -0,0 +1,174 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.1/font/bootstrap-icons.min.css" integrity="sha512-oAvZuuYVzkcTc2dH5z1ZJup5OmSQ000qlfRvuoTTiyTBjwX1faoyearj8KdMq0LgsBTHMrRuMek7s+CxF8yE+w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css"></head>
+</head>
+<body>
+<nav class="bg-primary d-flex justify-content-between p-3 ">
+  <div class="d-flex mx-4 mt-1 text-light">
+    <h5>Welcom! in electrolherba</h5>
+  </div>
+  <div class="mx-4 d-flex">
+    <a href="index.php" class="btn border-white border-1 text-light">LOG out</a>
+    <img width="30 px " height="40px" class="mx-3"  src="logo.png" alt="">
+
+  </div>
+
+ </nav>
+
+    <div class="d-flex justify-content-center mt-3 mb-4"><h4 class="text-light">Electro Naccer</h4></div>
+
+   
+   <?php 
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "Electro_naccer";
+
+    $connection = new mysqli($hostname, $username, $password, $database);
+
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+   
+    $result = $connection->query("SELECT * FROM products");
+    $category_list =  $connection->query("SELECT * FROM Category");
+   
+   ?>
+   <div class="d-flex ">
+<form method="post" style="margin-left:30px" class="  my-5 ">
+       
+        <select name="selectedCategory" id="categorySelect">
+            <?php
+           
+           echo '<option value="All" style="color: red; padding: 10px; font-size: 18px;">All</option>';
+            while ($productsCategory =   $category_list->fetch_assoc()) {
+                $categoryName = $productsCategory['Category_nam'];
+                echo '<option value="' . $categoryName . '" style="color: red; font-size: 18px;">' . $categoryName . '</option>';
+            }
+           
+            ?>
+        </select>
+
+        <button type="submit" class="btn bg-primary border-light"> Selecte </button>
+</form>
+<form method="post" style="" class="p-2  ">
+       
+        <select name="end-soon-products" id="end-soon-products">
+        <?php
+           
+            echo '<option  value="All" style="color: red; padding: 10px; font-size: 18px;">moins que la quantité minimale</option>';
+            
+            ?>
+        </select>
+
+        <button type="submit" class="btn bg-primary border-light"> select </button>
+</form>
+</div>
+
+
+    
+ 
+<?php  
+if (isset($_POST["end-soon-products"])) {
+    $filteredProductsByQuantity = $connection->query("SELECT * from Products where max_de_stok < mini_de_stok;");
+    display_products($filteredProductsByQuantity);
+} elseif (isset($_POST['selectedCategory'])) {
+    $selectedCategory = $_POST['selectedCategory'];
+    if ($selectedCategory == "All") {
+        display_products($result);
+    } else {
+        $filteredProducts = $connection->query("SELECT Products.Product_Id,Products.Product_name, Products.Product_img, Products.prix_unitair, Products.mini_de_stok, Products.max_de_stok, Category.Category_nam FROM Products JOIN Category ON Products.categoryy_ID = Category.Category_Id and Category.Category_nam = '$selectedCategory';");
+        display_products($filteredProducts);
+    }
+} else {
+    display_products($result);
+}
+?>
+ 
+
+
+
+
+  
+    
+<?php
+    function display_products($result)
+    {
+        echo '<div class="container-fluid row mx-auto my-4">';
+        
+        while ($product = $result->fetch_assoc()) {
+            $imagePath = $product['Product_img'];
+            $label = $product['Product_name'];
+            $unitPrice = $product['prix_unitair'];
+
+            // Apply spacing and styling to each card
+            echo '<div class="col-lg-3 col-md-4 col-sm-6 mb-3">';
+            echo '<div class="card p-3 h-100">';
+            echo '<img src="' . $imagePath . '" class="card-img-top h-75" alt="Product Image">';
+            echo '<div class="card-body">';
+            echo '<h5 class="card-title">' . $label . '</h5>';
+            echo '<p class="card-text">' . $unitPrice . ' DH</p>';
+            echo '</div></div></div>';
+        }
+        
+        echo '</div>';
+    }
+    ?>
+     <footer class="no-print bg-dark text-light">
+        <div class="card mt-5 mb-4"></div>  
+        <div class=" container tol MMM">
+      <div class="row  FFF ">
+      <div class=" col-lg-3  col-sm-4 col-6"><img class="w-50 " src="logo.png" alt=""></h5>
+        <P class="w-50">Savor the artistry where every dish is a culinary masterpiece</P>
+      
+      </div>
+      <div class="col-lg-3 col-sm-4 col-6">
+          <h6 >Useful links</h6>
+          <p>About us</p>
+          <p>Events</p>
+          <p>Blogs</p>
+          <p>FAQ</p>
+      </div>
+      <div class="  col-lg-3 col-sm-4 col-6">
+          <h6 >Main Menu</h6>
+          <p>Home</p>
+          <p>Menu</p>
+          <p>Blogs</p>
+          <p>Create</p>
+
+      </div>
+      <div class=" col-lg-3  col-sm-4 col-6">
+          <h6 >Contact Us</h6>
+          <p>electro naccer@email.coms</p>
+          <p>+64 958 248 966</p>
+          <p>Social media</p>
+       
+      </div>
+  </div>
+  <div   class="row " >
+      <div class="col-lg-5   ">
+  
+</div>
+  <div class="col-lg-5 mt-4  col-12 "><p >Copyright   2023 Dscode | All rights reserved</p></div>
+</div>
+  </div> 
+</footer>
+</body>
+</html>
+
+
+
+
+
+
+
